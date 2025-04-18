7786b0453016f7924da423a96afb7d1be5fa046f
Carlos09Or <ortizg.carlosa@gmail.com>
2025-04-17 22:55:57 -0500

    login,logout,signup. Nuevos html login y signup. 2 usuarios registrados

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,7 +1,42 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.urls import reverse_lazy
 from .models import Blog, Review, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+# Vista para el registro de usuarios
+class SignUpView(FormView):
+    template_name = 'blogapp/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('blogapp:blog_list')
+
+    def form_valid(self, form):
+        # Guarda el usuario y lo autentica
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)  # Usa el flujo estándar de FormView
+
+    def form_invalid(self, form):
+        # Renderiza errores en la plantilla
+        form.add_error(None, "Por favor, corrige los errores e intenta nuevamente.")
+        return super().form_invalid(form)
+
+class UserLoginView(LoginView):
+    template_name = 'blogapp/login.html'
+
+    def form_invalid(self, form):
+        # Agrega un mensaje de error genérico si las credenciales son incorrectas
+        form.add_error(None, "Usuario o contraseña incorrectos. Intenta de nuevo.")
+        return super().form_invalid(form)
+
+# Vista para cerrar sesión
+class UserLogoutView(LogoutView):
+    next_page = 'blogapp:blog_list'  # Redirige a la lista de blogs después de cerrar sesión
+
+#HACIA ARRIBA TODO ES NUEVO 
 
 class BlogListView(ListView):
     model = Blog
